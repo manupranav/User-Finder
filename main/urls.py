9fbd435a025f7d228d7c702c1000a335b35bd33a
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-08 12:22:25 -0700

    added login regisration

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import SearchDetail, SearchList, CustomLoginView, LogoutView, home
+from .views import RegisterView, SearchDetail, SearchList, CustomLoginView, LogoutView, home
 
 urlpatterns = [
+    path('register/',RegisterView.as_view(),name='register'),
     path('login/',CustomLoginView.as_view(),name='login'),
     path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
     path('',home,name='home'),
--- main/views.py
+++ main/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
- 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-from django.urls import reverse_lazy
 from .models import Search
 
 # Create your views here.
+
+
 
 class CustomLoginView(LoginView):
     template_name = 'main/login.html'
@@ -18,6 +23,23 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('home')
+
+class RegisterView(FormView):
+    template_name = 'main/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super(RegisterView, self).get( *args, **kwargs)
 
 
 def home(request):
@@ -30,7 +52,22 @@ class SearchList(ListView):
     template_name = 'main/search_list.html'
     context_object_name = 'searches'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['searches'] = context['searches'].filter(user=self.request.user)
+
+        return context
+
 class SearchDetail(DetailView):
     model = Search
     template_name = 'main/search.html'
     context_object_name = 'search'
+
+class SearchCreate(CreateView):
+    model = Search
+    fields = ['term']
+    success_url = reverse_lazy('')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(SearchCreate, self).form_valid(form)
