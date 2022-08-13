94b4e1d606fd358b82d34d837d2b83824202740c
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-12 20:45:54 -0700

    Updated Model

--- main/models.py
+++ main/models.py
@@ -1,11 +1,37 @@
+from email.policy import default
 from turtle import mode
+from unittest.mock import DEFAULT
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Search(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    term = models.CharField(max_length=100)
+
+
+class SearchTerm(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.term
+
+class SearchResult(models.Model):
+    CLAIMED   = "claimed"   # Username Detected
+    AVAILABLE = "available" # Username Not Detected
+    UNKNOWN   = "unknown"   # Error Occurred While Trying To Detect Username
+    ILLEGAL   = "illegal"   # This format of Username Not Allowable For This Site
+
+    SEARCH_CHOICES = (
+        (CLAIMED,   "Claimed"),
+        (AVAILABLE, "Available"),
+        (UNKNOWN,   "Unknown"),
+        (ILLEGAL,   "Illegal"),
+    )
+
+
+
+    term = models.ForeignKey(SearchTerm, on_delete=models.CASCADE)
+    sitename = models.CharField(max_length=50)
+    url = models.URLField(max_length=200)
+    search_status = models.CharField(max_length=20, choices=SEARCH_CHOICES, default=AVAILABLE)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
