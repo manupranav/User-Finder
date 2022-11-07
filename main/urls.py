36639abadd9fd8bf88ff8158fc639a72f50eebfd
Manu Pranav <manupranav62@gmail.com>
2022-11-07 19:39:47 +0530

    added crud urls

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,18 @@
 
 from django.urls import path, include
-from .views import RegisterView, SearchDetail, SearchList, CustomLoginView, LogoutView, home
+from .views import RegisterView, SearchDetail, SearchList, CustomLoginView, LogoutView,SearchUpdate, SearchDelete, SearchCreate, home, create_search
 
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
     path('login/',CustomLoginView.as_view(),name='login'),
     path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
+    path('create-search/',create_search,name='search-create'),
+    path('',SearchList.as_view(),name='search-list'),
+    path('search/<int:pk>',SearchDetail,name='search-detail'),
+    path('search-update/<int:pk>',SearchUpdate.as_view(),name='search-update'),
+    path('search-delete/<int:pk>',SearchDelete.as_view(),name='search-delete'),
     path('',home,name='home'),
+
+    path("__reload__/", include("django_browser_reload.urls")),
     
 ]
