d97f8e59c74970864e5eb1dbadb87104b09a8c9f
Manu Pranav <manupranav62@gmail.com>
2022-11-07 19:40:33 +0530

    fixed term not found

--- main/models.py
+++ main/models.py
@@ -5,19 +5,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class SearchTerm(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.term
+        return self.name
+
 
 class SearchResult(models.Model):
-    CLAIMED   = "claimed"   # Username Detected
-    AVAILABLE = "available" # Username Not Detected
-    UNKNOWN   = "unknown"   # Error Occurred While Trying To Detect Username
-    ILLEGAL   = "illegal"   # This format of Username Not Allowable For This Site
+    CLAIMED = "claimed"   # Username Detected
+    AVAILABLE = "available"  # Username Not Detected
+    UNKNOWN = "unknown"   # Error Occurred While Trying To Detect Username
+    ILLEGAL = "illegal"   # This format of Username Not Allowable For This Site
 
     SEARCH_CHOICES = (
         (CLAIMED,   "Claimed"),
@@ -26,15 +27,12 @@ class SearchResult(models.Model):
         (ILLEGAL,   "Illegal"),
     )
 
-
-
     term = models.ForeignKey(SearchTerm, on_delete=models.CASCADE)
     sitename = models.CharField(max_length=50)
     url = models.URLField(max_length=200)
-    search_status = models.CharField(max_length=20, choices=SEARCH_CHOICES, default=AVAILABLE)
+    search_status = models.CharField(
+        max_length=20, choices=SEARCH_CHOICES, default=AVAILABLE)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.term
-
-    
